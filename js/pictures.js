c818f93a5ab8cccec35725c9c2522107d0bb912a
Slava <ucheba00@gmail.com>
2018-04-09 16:05:27 -0600

    hotfix

--- js/pictures.js
+++ js/pictures.js
@@ -89,55 +89,59 @@ getPicturesArray(OBJECTS_COUNT);
 2,
 3 шаблон и отрисовка
 */
+var picturesRender = function () {
+  var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
+  var pictureList = document.querySelector('.pictures');
 
-var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
-var pictureList = document.querySelector('.pictures');
+  for (var i = 0; i < pictures.length; i++) {
+    var pictureElement = pictureTemplate.cloneNode(true);
 
-for (var i = 0; i < pictures.length; i++) {
-  var pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector('.picture__img').src = pictures[i].url;
+    pictureElement.querySelector('.picture__stat--likes').textContent = pictures[i].likes;
+    pictureElement.querySelector('.picture__stat--comments').textContent = pictures[i].comments.length;
 
-  pictureElement.querySelector('.picture__img').src = pictures[i].url;
-  pictureElement.querySelector('.picture__stat--likes').textContent = pictures[i].likes;
-  pictureElement.querySelector('.picture__stat--comments').textContent = pictures[i].comments.length;
-
-  pictureList.appendChild(pictureElement);
-}
+    pictureList.appendChild(pictureElement);
+  }
+};
+picturesRender();
 
 /*
 4 показать .big-picture
 */
+var bigPictureRender = function () {
+  var bigPicture = document.querySelector('.big-picture');
+  bigPicture.classList.remove('hidden');
 
-var bigPicture = document.querySelector('.big-picture');
-bigPicture.classList.remove('hidden');
+  var bigPictureSrc = bigPicture.querySelector('.big-picture__img img');
+  bigPictureSrc.src = pictures[0].url;
 
-var bigPictureSrc = bigPicture.querySelector('.big-picture__img img');
-bigPictureSrc.src = pictures[0].url;
+  var bigPictureLikes = bigPicture.querySelector('.likes-count');
+  bigPictureLikes.textContent = pictures[0].likes;
 
-var bigPictureLikes = bigPicture.querySelector('.likes-count');
-bigPictureLikes.textContent = pictures[0].likes;
+  var bigPictureCommentsCount = bigPicture.querySelector('.comments-count');
+  bigPictureCommentsCount.textContent = pictures[0].comments.length;
 
-var bigPictureCommentsCount = bigPicture.querySelector('.comments-count');
-bigPictureCommentsCount.textContent = pictures[0].comments.length;
+  var addComment = function () {
+    var comment = '';
+    for (var i = 0; i < pictures[0].comments.length; i++) {
+      comment += '<li class="social__comment social__comment--text">' +
+      '<img class="social__picture" src="img/avatar-' + getRandomInteger(1, 6) + '.svg" alt="Аватар комментатора фотографии" width="35" height="35">'
+      + pictures[0].comments[i] +
+      '</li>';
+    }
+    return comment;
+  };
 
-var addComment = function () {
-  var comment = '';
-  for (i = 0; i < pictures[0].comments.length; i++) {
-    comment += '<li class="social__comment social__comment--text">' +
-    '<img class="social__picture" src="img/avatar-' + getRandomInteger(1, 6) + '.svg" alt="Аватар комментатора фотографии" width="35" height="35">'
-    + pictures[0].comments[i] +
-    '</li>';
+  var bigPictureUl = bigPicture.querySelector('.social__comments');
+
+  while (bigPictureUl.hasChildNodes()) {
+    bigPictureUl.removeChild(bigPictureUl.firstChild);
   }
-  return comment;
+
+  bigPictureUl.insertAdjacentHTML('afterbegin', addComment());
 };
 
-var bigPictureUl = bigPicture.querySelector('.social__comments');
-
-while (bigPictureUl.hasChildNodes()) {
-  bigPictureUl.removeChild(bigPictureUl.firstChild);
-}
-
-bigPictureUl.insertAdjacentHTML('afterbegin', addComment());
-
+bigPictureRender();
 /*
 5 спрятать счетчик и новые комментарии
 */
